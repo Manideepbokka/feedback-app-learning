902e51f7a087f121ba36613babc0bdf3670f330e
Manideepbokka <saimanideep159@gmail.com>
2025-02-03 19:42:40 +0530

    Added changes related to routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from "./components/Header"
+import AboutIconLink from './components/AboutIconFile'
 import './App.css';
+import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Routes,  Route } from "react-router-dom";
 import { useState } from "react";
 import FeedbackData from './data/FeedbackData'
 import FeedBackList from './components/FeedbackItemList'
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from "./components/pages/AboutPage";
+
+
 function App() {
   const [feedback, setFeedback]=useState(FeedbackData)
   const deleteFeedBack=(id)=>{
@@ -13,16 +19,31 @@ function App() {
    }
   }
   const addNewFeedBack=(newFeed)=>{
+     newFeed.id=uuidv4();
      const newarr=[...feedback, newFeed ]
      setFeedback(newarr)
   }
   return (
-    <div >
+    <Router>
       <Header text="FeedBack UI"/>
-      <FeedbackForm addFeedBack={(newfeed)=> addNewFeedBack(newfeed)} />
-      <FeedbackStats feedback={feedback} />
-      <FeedBackList feedbackqwe={feedback} handleFeedBack={deleteFeedBack}/>
+    <div className="container" >
+    <Routes>
+    <Route 
+            path="/" 
+            element={
+              <>
+                <FeedbackForm addFeedBack={(newfeed) => addNewFeedBack(newfeed)} />
+                <FeedbackStats feedback={feedback} />
+                <FeedBackList feedbackqwe={feedback} handleFeedBack={deleteFeedBack} />
+              </>
+            } 
+          />
+      <Route path="/about" Component={AboutPage}/>
+      
+    </Routes>
+    <AboutIconLink/>
     </div>
+    </Router>
   );
 }
 
